1902dadda0596d47eeae06ad29e79f7235287d0d
laianebarbalho <laianebarbalho@gmail.com>
2022-11-18 11:30:15 -0300

    committing: testando branch laiane

--- src/main/java/com/dh/clinicaOdontologica/ClinicaOdontologicaApplication.java
+++ src/main/java/com/dh/clinicaOdontologica/ClinicaOdontologicaApplication.java
@@ -8,6 +8,7 @@ public class ClinicaOdontologicaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClinicaOdontologicaApplication.class, args);
+		//testando branch laiane
 	}
 
 }
